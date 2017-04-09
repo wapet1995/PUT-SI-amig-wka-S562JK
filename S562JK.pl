96c38b62642bc1f4495381c2eec94f226c94b24e
Maciej Marciniak <maciej.r.marciniak@student.put.poznan.pl>
2017-04-09 17:25:38 +0200

    wrzucenie wszystkiego co mam

---
+++ S562JK.pl
@@ -0,0 +1,16 @@
+:- use_module(library(clpfd)).
+
+zadanie([N1, DA1, DR1, S1], 
+		[N2, DA2, DR2, S2], 
+		[N3, DA3, DR3, S3], 
+		[N4, DA4, DR4, S4], 
+		[N5, DA5, DR5, S5],
+		[NAME1, NAME2, NAME3, NAME4, NAME5],
+		[DATA1, DATA2, DATA3, DATA4, DATA5],
+		[DRINK1, DRINK2, DRINK3, DRINK4, DRINK5],
+		[SONG1, SONG2, SONG3,SONG4, SONG5]):-
+			N1 = NAME1,
+			N2 = NAME2,
+			N3 = NAME3,
+			N4 = NAME4,
+			N5 = NAME5.
